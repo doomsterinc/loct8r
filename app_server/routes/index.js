7db53914fd9d940e363cd02c879c9affc3cf647e
doomsterinc <doomsterinc1@gmail.com>
2016-08-25 12:49:41 -0300

    add new controllers in MVC app with Express and NodeJs

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-
+/*var ctrlMain = require('../controllers/main'); */
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
