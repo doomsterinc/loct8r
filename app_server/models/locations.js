183364b7e89b845c4d18d3a4336775197207263b
doomsterinc <doomsterinc1@gmail.com>
2016-09-01 20:50:01 -0300

    define schema location /models/location.js

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,1 +1,8 @@
 var mongoose = require('mongoose');
+/* Location Schema */
+var locationSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  rating: {type: Number, "default": 0},
+  facilities: [String]
+});
