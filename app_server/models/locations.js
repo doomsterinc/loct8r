07070a4903d90d82c46061cf7be6bedc42294258
doomsterinc <doomsterinc1@gmail.com>
2016-09-01 20:58:10 -0300

    Update schema location /models/location.js 2

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var locationSchema = new mongoose.Schema({
   name: {type: String, required: true},
   address: String,
-  rating: {type: Number, "default": 0},
+  rating: {type: Number, "default": 0, min: 0, max: 5},
   facilities: [String]
 });
