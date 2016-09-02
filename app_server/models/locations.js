f726baeb63118ca20ed3dbc6ee485f0cfcd0cc5d
doomsterinc <doomsterinc1@gmail.com>
2016-09-02 18:08:04 -0300

    Update schema location /models/location.js 6 - Add compiling model from location schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -23,3 +23,5 @@ var locationSchema = new mongoose.Schema({
   openingTimes: [openingTimeSchema],
   reviews: [reviewSchema]
 });
+/* Compiling model from Schema Location */
+mongoose.model('Location', locationSchema);
