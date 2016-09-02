09646bc5974b62603c16c8d5eecb97f838dd2307
doomsterinc <doomsterinc1@gmail.com>
2016-09-02 17:28:36 -0300

    Update schema location /models/location.js 4 - Add review schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,4 +1,11 @@
 var mongoose = require('mongoose');
+/* Review Schema */
+var reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {type: Number, required: true, min: 0, max: 5}
+  reviewText: String,
+  createdOn: {type: Date, default: Date.now}
+});
 /* Location Schema */
 var locationSchema = new mongoose.Schema({
   name: {type: String, required: true},
