3c0d17259933556dca6eb35f4cb575e0f4fc5c67
doomsterinc <doomsterinc1@gmail.com>
2016-09-01 21:06:28 -0300

    Update schema location /models/location.js 3- Define coords uses GeoJSON

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -4,5 +4,6 @@ var locationSchema = new mongoose.Schema({
   name: {type: String, required: true},
   address: String,
   rating: {type: Number, "default": 0, min: 0, max: 5},
-  facilities: [String]
+  facilities: [String],
+  coords: {type: Number, index: '2dsphere'}
 });
