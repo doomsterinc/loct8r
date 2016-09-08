b30f2ae4edb5b81180ac6a5e89cee843639948f3
doomsterinc <doomsterinc1@gmail.com>
2016-09-08 14:06:10 -0300

    replace changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-/*var ctrlMain = require('../controllers/main'); */
-var ctrlLocations = require('../controllers/location');
+var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
-/* Locations pages  */
+
+/* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
-/* Others pages */
+
+/* Other pages */
 router.get('/about', ctrlOthers.about);
+
 module.exports = router;
