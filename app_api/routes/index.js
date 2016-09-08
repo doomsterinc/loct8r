87a1bc6e9b674249be46ff9cca8019f965389c3b
doomsterinc <doomsterinc1@gmail.com>
2016-09-08 11:03:49 -0300

    add metods Locations HTTP in app_api/routes/index

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,3 +2,10 @@ var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('..controllers/locations');
 var ctrReviews = require('..controllers/reviews');
+/* Locations */
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationsid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationsid', ctrlLocations.locationDeleteOne);
+// Reviews
