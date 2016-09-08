9222b727d388c5ff5552ddfeed8d98df221b0ac1
doomsterinc <doomsterinc1@gmail.com>
2016-09-08 11:35:31 -0300

    refactoring code

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,17 +1,17 @@
-// var express = require('express');
-// var router = express.Router();
-// var ctrlLocations = require('..controllers/locations');
-// var ctrlReviews = require('..controllers/reviews');
-// /* Locations */
-// router.get('/locations', ctrlLocations.locationsListByDistance);
-// router.post('/locations', ctrlLocations.locationsCreate);
-// router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-// router.put('/locations/:locationsid', ctrlLocations.locationsUpdateOne);
-// router.delete('/locations/:locationsid', ctrlLocations.locationDeleteOne);
-// // Reviews
-// router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-// router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-// router.put('/locations/:locationsid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-// router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
-//
-// module.exports = router;
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('..controllers/locations');
+var ctrlReviews = require('..controllers/reviews');
+/* Locations */
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationsid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationsid', ctrlLocations.locationDeleteOne);
+// Reviews
+router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
+router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
+router.put('/locations/:locationsid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
