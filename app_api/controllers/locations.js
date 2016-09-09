4f9478365242e040625d7cf6f9d0a4727cf3a1a9
doomsterinc <doomsterinc1@gmail.com>
2016-09-09 11:19:49 -0300

    Add reading data with mongoose

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -13,7 +13,22 @@ module.exports.locationsListByDistance = function(req, res) {
 
 /* GET a location by the id */
 module.exports.locationsReadOne = function(req, res) {
-  sendJSONresponse(res, 200, {'status': 'sucess'});
+  if(req.params && req.params.locationid){
+    Loc
+        .findById(req.params.locationid)
+        .exec(function(err, location){
+          if(!location){
+            sendJSONresponse(res, 404, {"message": "locationid not found"});
+            return;
+          }else if (err) {
+            sendJSONresponse(res, 404, err);
+            return;
+          }
+          sendJSONresponse(res, 200, location);
+        });
+  } else {
+    sendJSONresponse(res, 404, {"message" : "No locationid in request"});
+  }
 };
 
 /* POST a new location */
