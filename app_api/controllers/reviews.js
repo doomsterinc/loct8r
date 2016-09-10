15ba9acbd587b0e21256de035847896e6c5d8339
doomsterinc <doomsterinc1@gmail.com>
2016-09-10 11:05:05 -0300

    Add reading data with mongoose for reviews - problems resolved

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -8,39 +8,49 @@ var sendJSONresponse = function(res, status, content) {
 
 /* GET a review by the locationid */
 module.exports.reviewsReadOne = function(req, res) {
-  if(req.params && req.params.locationid && req.params.reviewid){
+  console.log("Getting single review");
+  if (req.params && req.params.locationid && req.params.reviewid) {
     Loc
-        .findById(req.params.locationid)
-        .select('name reviews')
-        .exec(function(err, location){
+      .findById(req.params.locationid)
+      .select('name reviews')
+      .exec(
+        function(err, location) {
+          console.log(location);
           var response, review;
-          if(!location){
-            sendJSONresponse(res, 404, {"message": "locationid not found"});
+          if (!location) {
+            sendJSONresponse(res, 404, {
+              "message": "locationid not found"
+            });
             return;
-          }else if (err) {
-            sendJSONresponse(res, 404, err);
+          } else if (err) {
+            sendJSONresponse(res, 400, err);
             return;
           }
-          if(location.reviews && location.reviews.lenght > 0){
+          if (location.reviews && location.reviews.length > 0) {
             review = location.reviews.id(req.params.reviewid);
             if (!review) {
-              sendJSONresponse(res, 404, {"message" : "reviewid not found"});
+              sendJSONresponse(res, 404, {"message": "reviewid not found"});
+            } else {
+              response = {
+                location: {
+                  name: location.name,
+                  id: req.params.locationid
+                },
+                review: review
+              };
+              sendJSONresponse(res, 200, response);
             }
           } else {
-            response = {
-              location: {
-                name : location.name,
-                id : req.params.locationid
-              },
-              review : review
-            };
-            sendJSONresponse(res, 200, response);
+            sendJSONresponse(res, 404, {
+              "message": "No reviews found"
+            });
           }
-        } else {
-          sendJSONresponse(res, 400, {"mesage" : "No reviews found"})
-        });
+        }
+    );
   } else {
-    sendJSONresponse(res, 404, {"message" : "No locationid in request"});
+    sendJSONresponse(res, 404, {
+      "message": "Not found, locationid and reviewid are both required"
+    });
   }
 };
 
