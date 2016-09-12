b938e146c439b8b611d22f4295d55a65c55afbe6
doomsterinc <doomsterinc1@gmail.com>
2016-09-12 08:18:30 -0300

    Add location update for put metods with app_api 2

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -127,7 +127,40 @@ module.exports.locationsUpdateOne = function(req, res) {
     });
     return;
   }
-  
+  Loc
+      .findById(req.params.locationid)
+      .select("-reviews -rating")
+      .exec(function(err, location){
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message" : "locationid not found"
+          });
+        } else if (err) {
+          sendJSONresponse(res, 400, err);
+        }
+        location.name = req.body.name;
+        location.address = req.body.address;
+        location.facilities = req.body.facilities.split(",");
+        location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+        location.openingTimes = [{
+          days : req.body.days1,
+          opening : req.body.opening1,
+          closing : req.body.closing1,
+          closed : req.body.closed1,
+        }, {
+          days : req.body.days2,
+          opening : req.body.opening2,
+          closing : req.body.closing2,
+          closed : req.body.closed2,
+        }];
+        location.save(function(err, location){
+          if (err) {
+            sendJSONresponse(res, 404, err);
+          } else {
+            sendJSONresponse(res, 200, location);
+          }
+        });
+      });
 };
 
 /* DELETE /api/locations/:locationid */
