6cec373d89e3f9df129bc986f89789c333c7e5f8
doomsterinc <doomsterinc1@gmail.com>
2016-09-14 10:36:34 -0300

    Treating Errors in data request 4

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -69,7 +69,15 @@ var _showError = function(req, res, status){
     if (status === 404) {
       title = "404, page not found";
       content = "Oh dear. Looks like we can't find this page. Sorry.";
+    } else {
+      title = status + ", something's gone wrong!";
+      content = "Something, somewhere, has gone just a little bit wrong.";
     }
+    res.status(status);
+    res.render('generic-text', {
+      title : tile,
+      content : content
+    });
 };
 
 /* GET 'home' page */
