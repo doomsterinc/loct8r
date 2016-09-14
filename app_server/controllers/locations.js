770d70da5a955a20352096036e87fc4334f89b01
doomsterinc <doomsterinc1@gmail.com>
2016-09-14 12:00:38 -0300

    Defined controllers of new reviews 4 - Add function to post data local review 2

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -162,4 +162,16 @@ module.exports.doAddReview = function(req, res) {
     rating : parseInt(req.body.rating, 10),
     reviewText : req.body.review
   };
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "POST",
+    json : postdata
+  };
+  request(requestOptions, function(err, response, body){
+    if (response.statusCode === 201) {
+      res.redirect('/location/' + locationid);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
+  });
 };
