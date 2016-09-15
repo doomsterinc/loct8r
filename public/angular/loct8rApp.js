2c7a1ab752bf80414da099b8a353978da7db7e9e
doomsterinc <doomsterinc1@gmail.com>
2016-09-15 12:16:38 -0300

    add dependencies angular in loct8r - modify the controller and passing data to controller angular

--- public/angular/loct8rApp.js
+++ public/angular/loct8rApp.js
@@ -1,1 +1,46 @@
 angular.module('loct8rApp', []);
+
+var locationListCtrl = function($scope){
+  $scope.data = {
+    locations: [{
+      name: 'Burguer Queen',
+      address : "125 High Street, Reading, RG6 1PS",
+      rating : 3,
+      facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+      distance : '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'}
+    },{
+      name: 'Cafe Queen',
+      address : "125 High Street, Reading, RG6 1PS",
+      rating : 3,
+      facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+      distance : '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'}
+    },{
+      name: 'Bistro Queen',
+      address : "125 High Street, Reading, RG6 1PS",
+      rating : 3,
+      facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+      distance : '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'}
+    },{
+      name: 'Brecho Queen',
+      address : "125 High Street, Reading, RG6 1PS",
+      rating : 3,
+      facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+      distance : '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'}
+    },{
+      name: 'Bangalo Queen',
+      address : "125 High Street, Reading, RG6 1PS",
+      rating : 3,
+      facilities : ['Hot Drinks', 'Food', 'Premium wifi'],
+      distance : '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'}
+    }]
+  };
+};
+
+angular
+    .module('loct8rApp')
+    .controller('locationListCtrl', locationListCtrl);
