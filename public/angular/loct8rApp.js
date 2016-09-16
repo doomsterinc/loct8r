7f3ed15e1a004a79e24d65b3afebef347ccb5985
doomsterinc <doomsterinc1@gmail.com>
2016-09-16 16:51:11 -0300

    format distance 2

--- public/angular/loct8rApp.js
+++ public/angular/loct8rApp.js
@@ -1,22 +1,25 @@
 angular.module('loct8rApp', []);
 
+// format distance
 var _isNumeric = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
-var _formatDistance = function (distance) {
-  var numDistance, unit;
-  if (distance && _isNumeric(distance)) {
-    if (distance > 1) {
-      numDistance = parseFloat(distance).toFixed(1);
-      unit = 'km';
+var formatDistance = function () {
+  return function (distance) {
+    var numDistance, unit;
+    if (distance && _isNumeric(distance)) {
+      if (distance > 1) {
+        numDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+      } else {
+        numDistance = parseInt(distance * 1000,10);
+        unit = 'm';
+      }
+      return numDistance + unit;
     } else {
-      numDistance = parseInt(distance * 1000,10);
-      unit = 'm';
+      return "?";
     }
-    return numDistance + unit;
-  } else {
-    return "?";
-  }
+  };
 };
 
 //console.log("Hello from the Angular");
@@ -70,4 +73,5 @@ var locationListCtrl = function($scope) {
 
 angular
     .module('loct8rApp')
-    .controller('locationListCtrl', locationListCtrl);
+    .controller('locationListCtrl', locationListCtrl)
+    .filter('formatDistance', formatDistance);
