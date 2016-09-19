6c83243d80f4401ab644a71194e1fd61afc7c539
doomsterinc <doomsterinc1@gmail.com>
2016-09-19 11:15:56 -0300

    data connect api with angular

--- public/angular/loct8rApp.js
+++ public/angular/loct8rApp.js
@@ -80,8 +80,54 @@ var locationListCtrl = function($scope) {
   };
 };
 
+var loct8rData = function(){
+  return [{
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '0.296456',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Costy',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 5,
+      facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+      distance: '0.7865456',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Cafe Hero',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 0,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '0.94561236',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 1,
+      facilities: ['Hot drinks', 'Food', 'Cold drinks'],
+      distance: '1.06548',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Simon\'s cafe',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '2.3654',
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      name: 'Sally\'s pub',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 5,
+      facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
+      distance: '4.213654',
+      _id: '5370a35f2536f6785f8dfb6a'
+    }];
+};
 angular
     .module('loct8rApp')
     .controller('locationListCtrl', locationListCtrl)
     .filter('formatDistance', formatDistance)
-    .directive('ratingStars', ratingStars);
+    .directive('ratingStars', ratingStars)
+    .service('loct8rData', loct8rData);
