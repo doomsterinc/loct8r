0329d4dba04da7e1f8a32743bc274545116a3439
doomsterinc <doomsterinc1@gmail.com>
2016-09-21 16:27:00 -0300

    Define services 1 - loct8rData

---
+++ app_client/common/services/loct8rData.service.js
@@ -0,0 +1,12 @@
+angular
+    .module('loct8rApp')
+    .service('loct8rData', loct8rData);
+
+function loct8rData ($http){
+  var locationByCoords = function(lat, lng){
+      return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20');
+  };
+  return {
+    locationByCoords : locationByCoords
+  };
+};
