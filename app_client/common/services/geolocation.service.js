78152e8cd14c2a0aa4a5b93717f9b36a46b1e179
doomsterinc <doomsterinc1@gmail.com>
2016-09-21 16:32:52 -0300

    Define services 1 - geolocation

---
+++ app_client/common/services/geolocation.service.js
@@ -0,0 +1,16 @@
+angular
+    .module('loct8rApp')
+    .service('geolocation', geolocation);
+
+function geolocation () {
+  var getPosition = function(cbSuccess, cbError, cbNoGeo) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(cbSuccess,cbError);
+    } else {
+      cbNoGeo();
+    }
+  };
+  return {
+    getPosition : getPosition
+  };
+};
