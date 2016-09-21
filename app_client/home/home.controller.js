52db0d0ef69bb3f34646b3ba8ed7e639e133bcdc
doomsterinc <doomsterinc1@gmail.com>
2016-09-21 15:37:35 -0300

    Create self controllers in angular 2

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -8,6 +8,6 @@ function homeCtrl ($scope) {
     strapline: 'Find places to work with wifi near you!'
   };
   $scope.sidebar = {
-    content : 'Looking for wifi and seat etc etc'
+    content : "Looking for wifi and a seat? loct8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let loct8r help you find the place you're looking for."
   };
 };
