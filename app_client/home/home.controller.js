90fd8380c65a4667777d874c81b5a6754aaf8e74
doomsterinc <doomsterinc1@gmail.com>
2016-09-21 16:18:21 -0300

    Define controllerAs

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -2,12 +2,13 @@ angular
     .module('loct8rApp')
     .controller('homeCtrl', homeCtrl);
 
-function homeCtrl ($scope) {
-  $scope.pageHeader = {
+function homeCtrl () {
+  var vm = this;
+  vm.pageHeader = {
     title: 'Loct8r',
     strapline: 'Find places to work with wifi near you!'
   };
-  $scope.sidebar = {
+  vm.sidebar = {
     content : "Looking for wifi and a seat? loct8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let loct8r help you find the place you're looking for."
   };
 };
