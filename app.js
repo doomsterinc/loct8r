6bb7076ebe9a4edcfcea8e0f844d89c3c2f4d6b2
doomsterinc <doomsterinc1@gmail.com>
2016-09-22 11:31:12 -0300

    Modify the route for new page of angular

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var fs = require('fs');
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 // var users = require('./app_server/routes/users');
-
 var app = express();
 
 // view engine setup
@@ -45,9 +44,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', routesApi);
 // app.use('/users', users);
+
+app.use(function(req, res){
+    res.sendfile(path.join(__dirname, 'app_client', 'index.html'))
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
