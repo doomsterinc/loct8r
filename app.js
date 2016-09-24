89a80e7a64bcf5df335bece711256571888d8730
doomsterinc <doomsterinc1@gmail.com>
2016-09-24 13:03:41 -0300

    Add about route 6

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var appClientFiles = [
   'app_client/common/services/loct8rData.service.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/filters/formatDistance.filter.js',
+  'app_client/common/filters/addHtmlLineBreaks.filter.js',
   'app_client/common/directive/ratingStars/ratingStars.directive.js',
   'app_client/common/directive/footerGeneric/footerGeneric.directive.js',
   'app_client/common/directive/navigation/navigation.directive.js',
