2b813977d441efe800b34813362f591efbc0c67e
doomsterinc <doomsterinc1@gmail.com>
2016-09-26 16:43:13 -0300

    add route location detail 4

--- app_client/app.js
+++ app_client/app.js
@@ -21,7 +21,7 @@
       })
       .otherwise({redirectTo: '/'});
 
-    $locationProvider.html5Mode(true);
+     $locationProvider.html5Mode(true);
   }
 
   angular
--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,12 +3,20 @@
       .module('loct8rApp')
       .controller('locationDetailCtrl', locationDetailCtrl);
 
-  locationDetailCtrl.$inject = ['$routeParams'];
-  function locationDetailCtrl($routeParams) {
+  locationDetailCtrl.$inject = ['$routeParams', 'loct8rData'];
+  function locationDetailCtrl($routeParams, loct8rData) {
     var vm = this;
     vm.locationid = $routeParams.locationid;
-    vm.pageHeader = {
-      title : 'Location Detail page'
-    };
+
+    loct8rData.locationById(vm.locationid)
+        .success(function(data){
+          vm.data = {location: data};
+          vm.pageHeader = {
+            title : vm.data.location.name
+          };
+        })
+        .error(function(e){
+          console.log(e);
+        });
   }
 })();
