765b0095d8153aeca9d7c419e1c22a496614a624
doomsterinc <doomsterinc1@gmail.com>
2016-10-10 17:53:33 -0300

    create modal angular ui 11

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -3,4 +3,13 @@
     .modlule('loct8rApp')
     .controller('reviewModal', reviewModal);
   reviewModal.$inject=['$modalInstance'];
+  function reviewModal($modalInstance) {
+    var vm = this;
+
+    vm.modal = {
+      cancel: function(){
+        $modalInstance.dismiss('cancel');
+      }
+    };
+  }
 })();
