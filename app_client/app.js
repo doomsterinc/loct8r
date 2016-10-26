ad909ab29c56d33d3c2e9eb92c6e25be4ce1410d
doomsterinc <doomsterinc1@gmail.com>
2016-10-26 19:39:20 -0200

    Add new route for login page

--- app_client/app.js
+++ app_client/app.js
@@ -1,32 +1,38 @@
 (function () {
 
-  angular.module('loct8rApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+  angular.module('loc8rApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
   function config ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'home/home.view.html',
+        templateUrl: '/home/home.view.html',
         controller: 'homeCtrl',
         controllerAs: 'vm'
       })
       .when('/about', {
-        templateUrl: 'common/views/genericText.view.html',
+        templateUrl: '/common/views/genericText.view.html',
         controller: 'aboutCtrl',
         controllerAs: 'vm'
       })
       .when('/location/:locationid', {
-        templateUrl: 'locationDetail/locationDetail.view.html',
+        templateUrl: '/locationDetail/locationDetail.view.html',
         controller: 'locationDetailCtrl',
         controllerAs: 'vm'
       })
       .when('/register', {
-        templateUrl: 'auth/register/register.view.html',
+        templateUrl: '/auth/register/register.view.html',
         controller: 'registerCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/login', {
+        templateUrl: '/auth/login/login.view.html',
+        controller: 'loginCtrl',
         controllerAs: 'vm'
       })
       .otherwise({redirectTo: '/'});
 
-     $locationProvider.html5Mode(true);
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
   }
 
   angular
